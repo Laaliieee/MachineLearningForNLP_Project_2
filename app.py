7d57a735ece91870dc17d334d8693192288cefbb
Joalie CORNELIE <joalie.cornelie@gmail.com>
2025-01-13 17:18:14 +0100

    Add Import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
